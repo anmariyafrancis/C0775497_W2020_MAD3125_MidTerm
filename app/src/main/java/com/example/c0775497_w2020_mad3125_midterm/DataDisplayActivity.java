6289df102764da3b777c0c4a29f8bfba64325ddd
anmariya <anmariya1915@gmail.com>
2020-04-09 18:58:02 -0400

    [ANMARIYA]calculated gross income

--- app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/DataDisplayActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/DataDisplayActivity.java
@@ -2,6 +2,8 @@ package com.example.c0775497_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -36,5 +38,59 @@ public class DataDisplayActivity extends AppCompatActivity {
         txtCpp = findViewById(R.id.txtCpp);
         txtTaxableIncome = findViewById(R.id.txtTotalTaxIncome);
         txtTaxPayed = findViewById(R.id.txtTaxPayed);
+
+        Intent mIntent = getIntent();
+        CRACustomer customer = mIntent.getParcelableExtra("CRACustomer");
+
+        txtSin.setText( customer.getSinNumber());
+        txtFullName.setText( customer.getFull_name());
+        txtGender.setText(getIntent().getStringExtra("gender"));
+        txtGrossIncome.setText(String.valueOf(customer.getGrossIncome()));
+        txtRrsp.setText(String.valueOf(customer.getRrspContribution()));
+
+        double grossIncome = customer.getGrossIncome();
+        if(grossIncome > 57400.00){
+            cpp = (57400.00 * 0.051); //5.10%
+        } else {
+            cpp = (grossIncome * 0.051);
+        }
+        txtCpp.setText(String.format("%.2f", cpp));
+
+        if(grossIncome > 53100){
+            ei = (53100 * 0.0162); //1.62%
+        }else{
+            ei = (grossIncome * (1.62/100));
+        }
+        txtEi.setText(String.format("%.2f", ei));
+
+        rrsp = customer.getRrspContribution();
+        double maxRRSP = (grossIncome * 0.18); //18%
+
+        rrspCarryFwd = maxRRSP - rrsp ;
+
+        if(rrspCarryFwd < 0)
+        {
+            lblRRSPCarryFwd.setTextColor(Color.RED);
+            lblRRSPCarryFwd.setText(String.format("%.2f",rrspCarryFwd));
+        }
+        else
+        {
+            lblRRSPCarryFwd.setText(String.format("%.2f",rrspCarryFwd));
+        }
+
+        taxableIncome = grossIncome - (cpp + ei + rrsp);
+        txtTaxableIncome.setText(String.format("%.2f",taxableIncome));
+
+        double calculateFederal = calculateFedralTax();
+        txtFederalTax.setText( String.format("%.2f",calculateFederal));
+
+        // Provincial Tax
+        double calculateProvincial = calculateProvincialTax();
+        txtProvincialTax.setText(String.format("%.2f",calculateProvincial));
+
+        //Total Tax Payed
+        totalTaxPaid = calculateFederal+ calculateProvincial;
+
+        lblTaxPayed.setText( String.format("%.2f",totalTaxPaid));
     }
 }

dfc2acee01e0fe93f5d6b2e0b816a7b00a44073d
anmariya <anmariya1915@gmail.com>
2020-04-09 17:59:59 -0400

    [ANMARIYA]getter and setter created in CRACustomer class

--- app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/CRACustomer.java
@@ -27,5 +27,125 @@ public class CRACustomer {
         this.grossIncome=grossIncome;
         this.rrspContribution=rrspContribution;
     }
+
+    public String getSinNumber() {
+        return sinNumber;
+    }
+
+    public void setSinNumber(String sinNumber) {
+        this.sinNumber = sinNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Date getTaxFilingDate() {
+        return taxFilingDate;
+    }
+
+    public void setTaxFilingDate(Date taxFilingDate) {
+        this.taxFilingDate = taxFilingDate;
+    }
+
+    public Double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(Double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public Double getFederalTax() {
+        return federalTax;
+    }
+
+    public void setFederalTax(Double federalTax) {
+        this.federalTax = federalTax;
+    }
+
+    public Double getProvincialTax() {
+        return provincialTax;
+    }
+
+    public void setProvincialTax(Double provincialTax) {
+        this.provincialTax = provincialTax;
+    }
+
+    public Double getCpp() {
+        return cpp;
+    }
+
+    public void setCpp(Double cpp) {
+        this.cpp = cpp;
+    }
+
+    public Double getEi() {
+        return ei;
+    }
+
+    public void setEi(Double ei) {
+        this.ei = ei;
+    }
+
+    public Double getRrspContribution() {
+        return rrspContribution;
+    }
+
+    public void setRrspContribution(Double rrspContribution) {
+        this.rrspContribution = rrspContribution;
+    }
+
+    public Double getTotalTaxableIncome() {
+        return totalTaxableIncome;
+    }
+
+    public void setTotalTaxableIncome(Double totalTaxableIncome) {
+        this.totalTaxableIncome = totalTaxableIncome;
+    }
+
+    public Double getTotalTaxPayed() {
+        return totalTaxPayed;
+    }
+
+    public void setTotalTaxPayed(Double totalTaxPayed) {
+        this.totalTaxPayed = totalTaxPayed;
+    }
 }
 

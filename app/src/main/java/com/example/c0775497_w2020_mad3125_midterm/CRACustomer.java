28a45fabc69e306278d9227db6b17be361669945
anmariya <anmariya1915@gmail.com>
2020-04-09 17:46:59 -0400

    [ANMARIYA]CRACustomer class created

---
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/CRACustomer.java
@@ -0,0 +1,21 @@
+package com.example.c0775497_w2020_mad3125_midterm;
+
+import java.util.Date;
+
+public class CRACustomer {
+    private String sinNumber;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String gender;
+    private String age;
+    private Date taxFilingDate;
+    private Double grossIncome;
+    private Double federalTax;
+    private Double provincialTax;
+    private Double cpp;
+    private Double ei;
+    private Double totalTaxableIncome;
+    private Double totalTaxPayed;
+}
+

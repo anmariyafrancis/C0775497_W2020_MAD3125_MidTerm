6ca8e810ec9e607e9b769461b24a76d3ee4bfaa0
anmariya <anmariya1915@gmail.com>
2020-04-09 19:01:18 -0400

    [ANMARIYA]defined code for calculating total tax

--- app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/DataDisplayActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/DataDisplayActivity.java
@@ -88,7 +88,6 @@ public class DataDisplayActivity extends AppCompatActivity {
         txtProvincialTax.setText(String.format("%.2f",calculateProvincial));
 
         totalTaxPaid = calculateFederal+ calculateProvincial;
-
         lblTaxPayed.setText( String.format("%.2f",totalTaxPaid));
     }
 }

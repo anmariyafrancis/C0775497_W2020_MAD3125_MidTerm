96e5ee4bc3cbb4e6ab87e036f5c5f3d4723292d1
anmariya <anmariya1915@gmail.com>
2020-04-09 14:46:12 -0400

    [ANMARIYA]variable for datepicker added

--- app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/PersonInfoEntryActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/PersonInfoEntryActivity.java
@@ -2,12 +2,15 @@ package com.example.c0775497_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class PersonInfoEntryActivity extends AppCompatActivity {
 
@@ -23,6 +26,13 @@ public class PersonInfoEntryActivity extends AppCompatActivity {
     private RadioButton rbtnFemale;
     private RadioButton rbtnOther;
     private RadioGroup rdGender;
+    private DatePickerDialog picker;
+
+    final Calendar calendar = Calendar.getInstance();
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    final   int month = calendar.get(Calendar.MONTH);
+    final int year = calendar.get(Calendar.YEAR);
+    String gender=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

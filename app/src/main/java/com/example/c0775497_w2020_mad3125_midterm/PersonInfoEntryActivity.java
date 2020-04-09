a4121748b9e0a4cbb7fe8038107d7183e6214a86
anmariya <anmariya1915@gmail.com>
2020-04-09 14:45:05 -0400

    [ANMARIYA]variables declared in PersonalEntryActivity.java

--- app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/PersonInfoEntryActivity.java
+++ app/src/main/java/com/example/c0775497_w2020_mad3125_midterm/PersonInfoEntryActivity.java
@@ -3,8 +3,26 @@ package com.example.c0775497_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class PersonInfoEntryActivity extends AppCompatActivity {
+
+    private Button btnCalculate;
+    private EditText edtFirstName;
+    private EditText edtLastName;
+    private EditText edtSinNumber;
+    private EditText edtDOB;
+    private TextView edtTaxFiledDate;
+    private EditText edtGrossIncome;
+    private EditText edtRRSP;
+    private RadioButton rbtnMale;
+    private RadioButton rbtnFemale;
+    private RadioButton rbtnOther;
+    private RadioGroup rdGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
